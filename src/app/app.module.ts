2de94f0efc460b63d2a7d45b943a531dfd1975a5
Kerstin Blumenstein <kblumenstein@fhstp.ac.at>
2017-11-02 15:50:37 +0100

    makes angular running in iOS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,18 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule, MatToolbarModule, MatMenuModule, MatIconModule, MatCardModule,
   MatFormFieldModule, MatInputModule} from '@angular/material';
 import { RouterModule, Routes } from '@angular/router';
+
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+import { AppRoutingModule } from './app-routing.module';
+
 import {BeaconService} from './beacon.service';
 
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { RegisterComponent } from './register/register.component';
 
-import { AppRoutingModule } from './app-routing.module';
-
-import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
-
 const socketConfig: SocketIoConfig = { url: 'http://god.meeteux.fhstp.ac.at:3000', options: {} };
-
-const appRoutes: Routes = [
-  { path: '', component: RegisterComponent},
-  { path: '**', component: PageNotFoundComponent }
-];
 
 @NgModule({
   declarations: [
