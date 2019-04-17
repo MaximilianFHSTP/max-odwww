a1376764a2163273dc140555f779d64b7a3be8a9
Kerstin Blumenstein <kblumenstein@fhstp.ac.at>
2019-04-17 16:34:29 +0200

    styling axis

--- src/app/components/main-view/main-view.component.ts
+++ src/app/components/main-view/main-view.component.ts
@@ -136,7 +136,8 @@ export class MainViewComponent implements OnInit, AfterViewInit, AfterViewChecke
         break;
       default:
         // stay here - TODO: CHANGE when pushing
-        /*this.router.navigate(['timelineStacked']).then( () =>
+        /*
+        this.router.navigate(['timelineStacked']).then( () =>
           {
             this.nativeCommunicationService.sendToNative('Timeline stacked', 'print');
           }
