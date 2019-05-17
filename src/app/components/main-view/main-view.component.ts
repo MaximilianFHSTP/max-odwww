533b87eb621ce56366a73c672d52ab0b3560c1d2
Kerstin Blumenstein <kblumenstein@fhstp.ac.at>
2019-05-17 11:21:05 +0200

    show normal timeline

--- src/app/components/main-view/main-view.component.ts
+++ src/app/components/main-view/main-view.component.ts
@@ -136,12 +136,12 @@ export class MainViewComponent implements OnInit, AfterViewInit, AfterViewChecke
         break;
       default:
         // stay here - TODO: CHANGE when pushing
-        
+        /*
         this.router.navigate(['timelineStacked']).then( () =>
           {
             this.nativeCommunicationService.sendToNative('Timeline stacked', 'print');
           }
-        );
+        );*/
         break;
     }
     
